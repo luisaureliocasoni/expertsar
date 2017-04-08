177b0c11632da92be33bd74acb04a170fdeb58b1
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-04-08 14:50:55 -0400

    colocação do sidenav e do footer no arquivo home.html

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+    //Ativa o sideNav na página
     $(".button-collapse").sideNav();
+    //Evento no botão do menu do Site
+    $("#btMenu").click(function(){
+        $('.button-collapse').sideNav('show');
+    });
 })
