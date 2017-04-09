289c80fc66544895ce4312d2c949a6eb4605081a
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-04-09 17:04:49 -0400

    Quarta versão da transição entre páginas do texto

--- js/lession.js
+++ js/lession.js
@@ -17,7 +17,6 @@ function carregaNextPage() {
     //Pega a próximo item a ser exibido, o irmão do atual
     var $next = $(".atual").next();
 
-    $("#next").addClass("disabled");
 
     //Oculta o atual
     $(".atual").hide().addClass("hide").removeClass("atual");
@@ -27,6 +26,12 @@ function carregaNextPage() {
     $next.show();
     //Marca o box como atual
     $next.addClass("atual");
+
+    //Verfica se a página que será exibida foi concluída, se não, desativa o botão
+    //Para evitar desativar o botão direto, use data-islession
+    if ($(".atual").data().concluido === undefined){
+        $("#next").addClass("disabled");
+    }
 }
 
 function carregaPreviousPage() {
@@ -54,8 +59,35 @@ function carregaPreviousPage() {
     $prev.addClass("atual");
 }
 
+function submitResposta($button) {
+    if ($button.data() === 0 || $button.data().for === undefined){
+        throw "Qual textarea devo consultar? Deve ser definido a proprieidade data-for no botao. Nada Feito.";
+    }
+
+    var $textarea = $($button.data().for);
+    if ($textarea.val() === "certo"){
+        $("#next").removeClass("disabled");
+        var $append = $(document.createElement("div"));
+        $append.addClass("col s12 focus");
+        $append.append("<div class=\"final\"><p>Query SQL</p><code class=\"final\">A query SQL aparecerá aqui</code></div>");
+        $append.append("<div class=\"border final\"><table class=\"responsible-table striped centered\"><thead><tr><th>nomecli</th></tr><tbody><tr><td>João</td></tr><tr><td>Marcelo</td></tr><tr><td>(...)</td></tr></tbody></div>");
+        $(".atual").append($append);
+        //Marca a lição como concluido
+        $(".atual").data("concluido", "yes");
+        Materialize.toast("Resposta Correta!", 4000);
+        $button.addClass("disabled");
+        //$(".focus").focus();
+    }else{
+        Materialize.toast("Resposta Incorreta!", 4000);
+    }
+
+}
+
 $(document).ready(function(){
     $("#next").click(carregaNextPage);
     $("#prev").click(carregaPreviousPage);
+    $(".submit").click(function () {
+        submitResposta($(this));
+    });
     defineInsertButtonEvent();
 });
