1d45aa7ea21329d5cb44ad26c2e7d4a94212ccd6
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-04-26 14:17:14 -0400

    Inclusão da função insert na classe DAO

--- src/lib/DAO.php
+++ src/lib/DAO.php
@@ -33,15 +33,71 @@ namespace lib;
  * @author root
  */
 class DAO {
-    private $conn;
+    private static $conn;
     private static $instance;
     
-    private function __construct($conn) {
-        $this->conn = $conn;
+    private function __construct() {
+        
     }
     
-    public function insert($obj){
+    private static function initialize() {
+        try{
+            $config = parse_ini_file("../../assets/conexao.ini");
+            if ($config === FALSE){
+                throw new \Exception("Não foi possível localizar as configurações do BD.");
+            }
+            self:$conn = pg_connect("host={$config["server"]} dbname={$config["database"]} user={$config["user"]} password={$config["password"]}");
+            if (self::$conn === FALSE){
+                throw new \Exception("Não foi possível conectar ao Banco de Dados");
+            }
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
+    }
+    
+    public function insert($obj, $ignoreNulls = TRUE){
+        if (self::$conn  != NULL){
+            pg_close(self::$conn);
+        }
+        self::initialize();
+        //Pega os dados do objeto, a procura de getters
+        $reflectObj = new \ReflectionClass($obj);
+        $colunas = new \ArrayObject();
+        $valores = new \ArrayObject();
+        foreach ($reflectObj->getMethods() as $method){
+            //Pega todos os métodos, procurando gets
+            if (strpos($method->name, "get") === 0){
+                //Invoca o get
+                $value = $method->invoke($obj);
+                //Remove o get e transforma a primeira letra do atributo em minusculo
+                $attr = lcfirst(str_replace("get", "", $method->name));
+                if ($value !== \NULL || $ignoreNulls === \FALSE ){
+                    $colunas->append($attr);
+                    $valores->append(self::toStr($value));
+                }
+                
+            }
+        }
+        $table = $reflectObj->getProperty("table")->getValue($obj);
         
+        $query = "INSERT INTO $table (".implode(",", $colunas).")";
+        $query .= "VALUES (". implode(",", $valores).";";
+        
+        $result = pg_query(self::$conn, $query);
+        if ($result === FALSE){
+            throw new \Exception("Erro na query: "+pg_last_error(self::$conn));
+        }
+    }
+    
+    private static function toStr($x){
+        $type = gettype($x);
+        if ($type === "string" || $type === "integer" || $type === "double"){
+            return pg_escape_string($x);
+        }else if ($type === "boolean") {
+            return ($x ? "TRUE" : "FALSE");
+        }else{
+            return $type;
+        }
     }
 
 }
