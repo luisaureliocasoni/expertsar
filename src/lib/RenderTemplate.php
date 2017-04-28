a4abd2767e449ec7ae7b911cdd3c52b8fb5df978
Luis Aurelio Casoni <luis_aurelio96@live.com>
2017-04-27 22:04:04 -0400

    Inclusão da classe RenderTemplate, interface com o Twig

---
+++ src/lib/RenderTemplate.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * ExpertsAR - Um Objeto de Aprendizagem para Álgebra Relacional
+ * (C) 2017 - Luís Aurélio Casoni e Ademir Martinez Sanches
+ * Este código-fonte está licenciado sob a licença MIT.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * */
+
+namespace lib;
+
+/**
+ * Renderiza um templata Twig
+ *
+ * @author Luís Aurélio Casoni
+ */
+class RenderTemplate{
+    private $loader;
+    private $twig;
+    
+    /**
+     * Construtor da classe
+     * @param string $path Path para encontrar a pasta de templates HTML do Twig
+     */
+    public function __construct($path = '../view'){
+        $this->loader = new \Twig_Loader_Filesystem($path);
+        $this->twig = new \Twig_Environment($this->loader, array());
+    }
+    
+    /**
+     * Renderiza o template
+     * @param string $page String com o nome da página a ser renderizada
+     * @param array $array Array com os dados a serem exibidos na página
+     */
+    public function render(string $page, array $array = null){
+        if ($array == null){
+            echo $this->twig->render($page);
+        }else{
+            echo $this->twig->render($page, $array);
+        }
+    }
+}
