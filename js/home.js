c3153556ab4015a2f4ac9331f2ecab6e0a32a183
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-05-01 11:02:58 -0400

    Implemetação final da interface de remoção, faltando agora implementar os scripts de remoções

--- js/home.js
+++ js/home.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
         var id = $(this).data("id");
         var type = $(this).data("type");
         var resposta;
-        resposta = window.alert("Tem certeza que deseja remover esta "+type+"?");
-        console.log("resposta");
+        resposta = window.confirm("Tem certeza que deseja remover esta "+type+"?\nA ação não pode ser revertida!");
+        if (resposta === true){
+            window.location = page+"?id="+id;
+        }
     });
     //Ativa o sideNav na página
     $(".button-collapse").sideNav();
