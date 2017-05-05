633a66133224b3de8c84943ae74136e14b1992e5
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-05-05 15:13:06 -0400

    Criação preliminar do mecanismo de criação de conta

--- createConta.php
+++ createConta.php
@@ -28,16 +28,65 @@ try{
     require_once("vendor/autoload.php");
     $render = new \Lib\RenderTemplate("view/");
     $sessao = new \Lib\SessionManager();
-    
+    $info = [];
     if ($sessao->keyExists("logado")){
         //Se está logado, obviamente tem que redirecionar ao indice
         header("Location: index.php");
         die();
     }
     
+    //geracao do captcha
+    $info["op1"] = \Lib\DAOUtilis::sorteiaNumero();
+    $info["op2"] = \Lib\DAOUtilis::sorteiaNumero();
+    $info["operador"] = \Lib\DAOUtilis::sorteiaOperador();
     
+    if (isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"]) && isset($_POST["senhaConfirm"]) && isset($_POST["captcha"])){
+        $info["errors"] = "";
+        
+        if (strlen($_POST["nome"]) === 0){
+            $info["errors"] .= "<p>O nome deve ser preenchido!</p>";
+        }
+        
+        if (strlen($_POST["senha"]) < 8 || strlen($_POST["senhaConfirm"]) < 8){
+            $info["errors"] .= "<p>A senha deve ter oito caracteres ou mais!</p>";
+        }
+        
+        if ($_POST["senha"] !== $_POST["senhaConfirm"]){
+            $info["errors"] .= "<p>As senhas não batem!</p>";
+        }
+        
+        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === FALSE){
+            $info["errors"] = "<p>O e-mail fornecido é inválido!</p>";
+        }
+        
+        if (filter_var($_POST["captcha"], FILTER_VALIDATE_INT) === FALSE){
+            $info["errors"] = "<p>A resposta do desafio fornecido é inválido!</p>";
+        }
+        
+        $eq = $_POST["operando1"].$_POST["operador"].$_POST["operando2"];
+        if ($_POST["captcha"] != jlawrence\eos\Parser::solve($eq)){
+            $info["errors"] = "<p>A resposta do desafio fornecido é incorreta!</p>";
+        }
+        
+        if(!(isset($_POST["aceitaTermos"])) && ($_POST["aceitaTermos"] !== "sim")){
+            $info["errors"] = "<p>Por favor, aceite nossos termos de uso para prosseguir.</p>";
+        }
+        
+        
+        
+        if ($info["errors"] === ""){ //se não houve erros salva o dado e redireciona para o login
+            $user = new ExpertsAR\Usuario();
+            $user->setEmail(Lib\DAO::escapeString($_POST["email"]));
+            $user->setNome(Lib\DAO::escapeString($_POST["nome"]));
+            $user->setPass(\Lib\DAOUtilis::criptografaSenha($_POST["senha"]));
+            Lib\DAO::insert($user);
+            
+            header("Location: login.php?icode=1");
+            die();
+        }
+    }
     //Renderiza página
-    $render->render("createConta.html");
+    $render->render("createConta.html", $info);
     
 } catch (Exception $ex) {
     $handler = new \Lib\ExceptionHandler($ex, "createConta.php");
