bf85c4d26afe8b274176546ebf83558f3eca7c04
Luís Aurélio Casoni <luis_aurelio96@live.com>
2017-05-20 16:45:36 -0400

    Inclusão do arquivo help.php

---
+++ help.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * ExpertsAR - Um Objeto de Aprendizagem para Álgebra Relacional
+ * (C) 2017 - Luís Aurélio Casoni e Ademir Martinez Sanches
+ * Este código-fonte está licenciado sob a licença MIT.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+try{
+    require_once("vendor/autoload.php");
+    
+    $sessao = new \Lib\SessionManager();
+    $render = new Lib\RenderTemplate("view/");
+
+    $info = [];
+    
+    if ($sessao->keyExists("logado")){
+       
+        $info = $info + $sessao->getAllKeys();
+        //seta a key de fazendo licao como null
+        $sessao->addKey("fazendoLicao", NULL);
+        
+        $info["primeiroNome"] = explode(" ", $_SESSION["nome"])[0];
+    }
+    $render->render("help.html", $info);
+} catch (Exception $ex) {
+    $handler = new \Lib\ExceptionHandler($ex, basename($_SERVER['PHP_SELF']));
+    $handler->run();
+}
